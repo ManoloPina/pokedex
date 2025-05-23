c864e56e9fa708721c96f2fbd63fad55fdd7da52
Manolo Pina <manolobarbosapina@gmail.com>
2025-05-22 23:13:54 -0300

    #3 - Main Layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import './globals.css';
 import { twMerge } from 'tailwind-merge';
-
-import Image from 'next/image';
 import { Montserrat } from 'next/font/google';
 import type { Metadata } from 'next';
+import Providers from '@/context/QueryClientProvider';
+
+import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import Sidebar from '@/components/Sidebar';
+import SearchBar from '@/components/SearchBar';
 
 export const metadata: Metadata = {
   title: 'Pokedex - Home Page',
@@ -22,60 +25,66 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
-        <header
-          className={twMerge(
-            montserrat.className,
-            'w-full h-screen max-h-[600px] bg-radial from-[#AB0001] from-0% to-[#AB0001] to-100% relative'
-          )}
-        >
-          <div className="flex flex-col h-full max-w-7xl mx-auto justify-center items-center">
-            <div className="w-full pt-5">
-              <Image
-                alt="Pokemon Logo"
-                src="/logo.svg"
-                width={159}
-                height={58}
-              />
-            </div>
-            <div className="absolute bottom-1/2">
-              <h2 className="text-white font-bold text-[6.4rem]">
-                Who&apos;s that Pokémon?
-              </h2>
-              <p className="text-lg font-medium text-white text-center">
-                The perfect guide for those who want to hunt Pokémon around the
-                world.
-              </p>
-            </div>
-            <Button
-              variant="secondary"
-              className="rounded-full text-blue-500 font-semibold text-sm hover:bg-secondary"
-            >
-              <div className="rounded-full bg-blue-200 p-0.5">
+      <Providers>
+        <body className={`antialiased`}>
+          <header
+            className={twMerge(
+              montserrat.className,
+              'w-full h-screen max-h-[600px] bg-radial from-[#AB0001] from-0% to-[#AB0001] to-100% relative'
+            )}
+          >
+            <div className="flex flex-col h-full max-w-7xl mx-auto justify-center items-center">
+              <div className="w-full pt-5">
                 <Image
-                  src="/pokedex-bag.png"
-                  alt="Pokédex Bag"
-                  width={16}
-                  height={16}
+                  alt="Pokemon Logo"
+                  src="/logo.svg"
+                  width={159}
+                  height={58}
                 />
               </div>
-              Pokédex
-            </Button>
-            <div
-              style={{ backgroundImage: "url('/red-bg-pattern.png')" }}
-              className="bg-center bg-no-repeat bg-contain w-full mx-auto flex-1"
-            />
-            <Image
-              className="absolute -bottom-[248px]"
-              src="/red-pokeball.png"
-              alt="Red Pokeball"
-              width={800}
-              height={520}
-            />
+              <div className="absolute bottom-1/2">
+                <h2 className="text-white font-bold text-[6.4rem]">
+                  Who&apos;s that Pokémon?
+                </h2>
+                <p className="text-lg font-medium text-white text-center">
+                  The perfect guide for those who want to hunt Pokémon around
+                  the world.
+                </p>
+              </div>
+              <Button
+                variant="secondary"
+                className="rounded-full text-blue-500 font-semibold text-sm hover:bg-secondary"
+              >
+                <div className="rounded-full bg-blue-200 p-0.5">
+                  <Image
+                    src="/pokedex-bag.png"
+                    alt="Pokédex Bag"
+                    width={16}
+                    height={16}
+                  />
+                </div>
+                Pokédex
+              </Button>
+              <div
+                style={{ backgroundImage: "url('/red-bg-pattern.png')" }}
+                className="bg-center bg-no-repeat bg-contain w-full mx-auto flex-1"
+              />
+              <Image
+                className="absolute -bottom-[248px]"
+                src="/red-pokeball.png"
+                alt="Red Pokeball"
+                width={800}
+                height={520}
+              />
+            </div>
+          </header>
+          <SearchBar />
+          <div className="grid grid-cols-[1fr_2fr] max-w-7xl mx-auto items-start justify-start">
+            <Sidebar className=" border border-green-500" list={[]} />
+            <div className="border border-pink-600 ">{children}</div>
           </div>
-        </header>
-        {children}
-      </body>
+        </body>
+      </Providers>
     </html>
   );
 }
---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { twMerge } from 'tailwind-merge';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Search } from 'lucide-react';
+import { useState } from 'react';
+
+interface IProps {
+  className?: string;
+}
+
+export default function SearchBar({ className = '' }: Readonly<IProps>) {
+  const [searchText, setSearchText] = useState('');
+  return (
+    <div
+      className={twMerge(
+        'h-[300px] bg-gradient-to-tr from-[#eff3f6]/100 to-[#eff3f6]/10 flex items-center',
+        className
+      )}
+    >
+      <div className="max-w-7xl mx-auto w-full flex flex-row items-center justify-between">
+        <p className="font-bold text-primary text-3xl max-w-[260px]">
+          Selecione o seu Pokémon
+        </p>
+        <div className="w-1/3">
+          <Input
+            className="z-10 rounded-full text-sm placeholder:text-[#A0AFBA] border-none shadow-none bg-white w-full !px-6 !py-4 h-[56px]"
+            onChange={(e) => setSearchText(e.target.value)}
+            value={searchText}
+            placeholder="Pesquise pelo nome ou código"
+            endAdornment={
+              <Button
+                className="rounded-full bg-[#9EB9E1] hover:bg-[#9EB9E1]/90 size-[42px] cursor-pointer"
+                onClick={() => {}}
+              >
+                <Search className="text-[#3E75C3] font-bold" size={16} />
+              </Button>
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
