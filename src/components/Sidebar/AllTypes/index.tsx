7e4a317450f6f5c70622fe150cc2312d7ca85548
Manolo Pina <manolobarbosapina@gmail.com>
2025-06-16 22:54:29 -0300

    #9 - Pokemon List
    - Implement active element for All Types selected

--- src/components/Sidebar/AllTypes/index.tsx
+++ src/components/Sidebar/AllTypes/index.tsx
@@ -2,12 +2,21 @@
 
 import Link from 'next/link';
 import { PokeBall } from '@/assets/icons';
+import { cn } from '@/lib/utils';
+import { useSearchParams } from 'next/navigation';
 
 export default function AllTypes() {
+  const searchParams = useSearchParams();
+
+  const selectedType = searchParams.get('type');
+  const isActive = !selectedType;
   return (
     <Link
       href="/"
-      className="font-semibold text-sm text-[#ACB9C1] hover:text-[#3E75C3] cursor-pointer flex flex-row items-center gap-6.5"
+      className={cn(
+        'font-semibold text-sm text-[#ACB9C1] hover:text-[#3E75C3] cursor-pointer flex flex-row items-center gap-6.5',
+        isActive ? 'text-[#3E75C3]' : 'text-[#ACB9C1]'
+      )}
     >
       <PokeBall className="size-4 align-middle text-[#3E75C3]" />
       All types
